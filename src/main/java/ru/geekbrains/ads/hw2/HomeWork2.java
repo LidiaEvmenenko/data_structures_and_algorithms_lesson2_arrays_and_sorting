37c1b04cdbb1d713483cc5b0664e31f2814d1dba
LidiaEvmenenko <inu-chan@mail.ru>
2021-09-16 20:56:41 +0500

    Домашнее задание по второй лекции

--- src/main/java/ru/geekbrains/ads/hw2/HomeWork2.java
+++ src/main/java/ru/geekbrains/ads/hw2/HomeWork2.java
@@ -10,17 +10,27 @@ import java.util.function.UnaryOperator;
 public class HomeWork2 {
 
     public static void main(String[] args) {
-        int n = 10;
+        int n = 100_000;
         Random random = new Random();
-        MyArrayList<Integer> mal = new MyArrayList<>(n);
+        MyArrayList<Integer> malSS = new MyArrayList<>(n);
+        MyArrayList<Integer> malIS = new MyArrayList<>(n);
+        MyArrayList<Integer> malBS = new MyArrayList<>(n);
+        int rand;
         for (int i = 0; i < n; i++) {
-            mal.add(random.nextInt(10));
+            rand=random.nextInt(n);
+            malSS.add(rand);
+            malIS.add(rand);
+            malBS.add(rand);
         }
-        System.out.println(mal);
-//        mal.selectionSort();
-//        mal.insertionSort();
-        mal.bubbleSort();
-        System.out.println(mal);
+        long a = System.currentTimeMillis();
+        malSS.selectionSort();
+        System.out.println("Время работы сортировки методом выбора: "+(System.currentTimeMillis() - a)+"ms.");
+        a = System.currentTimeMillis();
+        malIS.insertionSort();
+        System.out.println("Время работы сортировки вставками: "+(System.currentTimeMillis() - a)+"ms.");
+        a = System.currentTimeMillis();
+        malBS.bubbleSort();
+        System.out.println("Время работы пузырьковой сортировки: "+(System.currentTimeMillis() - a)+"ms.");
     }
 
 }
